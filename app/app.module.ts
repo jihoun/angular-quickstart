1ed6dcde2ff3f4b9c817b88e5614156e98ea6ee0
Nicolas Lagier <nicolas.lagier@gmail.com>
2016-10-08 08:48:51 +0800

    with http

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,47 +1,55 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule }   from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent }   from './app.component';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }   from './dashboard.component';
-import { HeroService }         from './hero.service';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
       {
         path: '',
         redirectTo: '/dashboard',
         pathMatch: 'full'
       },
       {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
         path: 'detail/:id',
         component: HeroDetailComponent
       },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
     ])
   ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
   ],
   providers: [
-    HeroService
+    HeroService,
   ],
-  bootstrap:    [ AppComponent ],  
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
